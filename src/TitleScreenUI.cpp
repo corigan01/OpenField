1c070e9163f4d1cb06083fc3fa67871820e418f9
corigan01 <corigan01@gmail.com>
2023-11-04 15:01:59 -0500

    render spinning holy image

--- src/TitleScreenUI.cpp
+++ src/TitleScreenUI.cpp
@@ -6,13 +6,21 @@
 
 TitleScreenUI::TitleScreenUI() :
         mainTitle { "OpenField", 300, 100, 45, BLACK },
-        playGame { 300, 300, 200, 100 } {
+        playGame { 300, 300, 200, 100 },
+        background { "../textures/holy.png", {0, 0, 800 * 2, 800 * 2} } {
 
+    background.change_target({800 / 2, 800 / 2, 800 * 2, 800 * 2});
+    background.set_origin({800, 800});
+
+    this->add_element(&background);
     this->add_element(&playGame);
     this->add_element(&mainTitle);
 }
 
 void TitleScreenUI::update_event() {
+    float rot = this->background.get_rotation();
+    this->background.change_rotation(rot + 0.05);
+
     if (playGame.is_clicked()) {
         next_stage = "play";
     }
