f4b0bd830521bb337e16b18147316a9306f5ab16
corigan01 <corigan01@gmail.com>
2023-11-03 22:53:06 -0500

    Included ui class into the main method to handle updates

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,32 @@
 #include <raylib.h>
+#include <UI.h>
+#include <SimpleUIElement.h>
 
 int main(void) {
   const int screenWidth = 800;
   const int screenHeight = 450;
 
-  InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+  InitWindow(screenWidth, screenHeight, "OpenField");
+  SetTargetFPS(60);
+
+  UI ui {};
 
   SetTargetFPS(60); // Set our game to run at 60 frames-per-second
 
-  while (!WindowShouldClose()) // Detect window close button or ESC key
+  // ----- END ADD YOUR CLASSES HERE ----
+
+  ui.init_all();
+
+  while (!WindowShouldClose())
   {
-    BeginDrawing();
+      ui.updateUI();
 
-    ClearBackground(RAYWHITE);
+      BeginDrawing();
+      ClearBackground(RAYWHITE);
 
-    DrawText("Congrats! You created your first window!", 190, 200, 20,
-             LIGHTGRAY);
+      ui.draw2D_all();
 
-    EndDrawing();
+      EndDrawing();
   }
 
   CloseWindow(); // Close window and OpenGL context
