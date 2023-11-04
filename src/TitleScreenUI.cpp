6130e9cc602e39bd4f90eed64347a41864fadf1f
corigan01 <corigan01@gmail.com>
2023-11-04 02:28:15 -0500

    Construct title screen that swiches state

---
+++ src/TitleScreenUI.cpp
@@ -0,0 +1,23 @@
+//
+// Created by corigan01 on 11/4/23.
+//
+
+#include "TitleScreenUI.h"
+
+TitleScreenUI::TitleScreenUI() :
+        mainTitle { "OpenField", 300, 100, 45, BLACK },
+        playGame { 300, 300, 200, 100 } {
+
+    this->add_element(&playGame);
+    this->add_element(&mainTitle);
+}
+
+void TitleScreenUI::update_event() {
+    if (playGame.is_clicked()) {
+        next_stage = "play";
+    }
+}
+
+const char* TitleScreenUI::finished_event_control() {
+    return next_stage;
+}
