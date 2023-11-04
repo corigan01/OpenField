4d2872ba52c3adfdf9ba9c42235868915b5d47ef
corigan01 <corigan01@gmail.com>
2023-11-04 15:01:33 -0500

    Include more Texture options

--- src/TextureUIElement.h
+++ src/TextureUIElement.h
@@ -8,9 +8,9 @@
 
 class TextureUIElement : public UIElement {
 private:
-    Texture2D texture;
-    Rectangle src, target = {};
-    Vector2 org = {};
+    Texture2D texture {};
+    Rectangle src {}, target {};
+    Vector2 org {};
     float rotation = 0;
     Color tint = WHITE;
 
@@ -18,14 +18,21 @@ private:
         Image def = GenImageChecked(target.width, target.height, 10, 10, MAGENTA, BLACK);
         texture = LoadTextureFromImage(def);
         UnloadImage(def);
-        org = { target.width / 2, target.height / 2};
+        org = { 0, 0};
     }
 
 public:
-    TextureUIElement(Rectangle drawing_pos) {
+    explicit TextureUIElement(Rectangle drawing_pos) {
+        src = {0, 0, drawing_pos.width, drawing_pos.height};
         target = drawing_pos;
-        src = drawing_pos;
         generate_def();
+    }
+
+    TextureUIElement(const char* filename, Rectangle drawing_pos) {
+        Image def = LoadImage(filename);
+        texture = LoadTextureFromImage(def);
+        org = {0, 0};
+        src = {0, 0, static_cast<float>(def.width), static_cast<float>(def.height)};
     }
 
     ~TextureUIElement() = default;
@@ -34,6 +41,37 @@ public:
         DrawTexturePro(texture, src, target, org, rotation, tint);
     }
 
+    void change_rotation(float rot) {
+        this->rotation = rot;
+    }
+
+    void change_target(Rectangle trg) {
+        this->target = trg;
+    }
+
+    float get_rotation() {
+        return rotation;
+    }
+
+    Rectangle get_target() {
+        return target;
+    }
+
+    Vector2 get_origin() {
+        return org;
+    }
+
+    void set_origin(Vector2 origin) {
+        org = origin;
+    }
+
+    Rectangle get_src() {
+        return src;
+    }
+
+    void set_src(Rectangle source) {
+        src = source;
+    }
 };
 
 
